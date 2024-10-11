87f0b844a3a845dc9643486300b5a298054f0ae9
Rundus <connorfeltman@yahoo.com>
2024-10-11 13:46:25 -0500

    spaceToolsLib_package commit 5

--- setupFuncs/data_paths.py
+++ setupFuncs/data_paths.py
@@ -7,4 +7,9 @@ user = 'cfelt'
 PATH_TO_DATA_FOLDER = r'C:\Data\\'
 HOMEDRIVE = 'C:'
 HOMEPATH = 'C:\\'
-CDF_LIB = rf"C:\Users\cfelt\PycharmProjects\CDF\lib" # location to directory that contains the dllcdf.dll needed for pycdf the library
+
+# CDF LIB path
+from pathlib import Path
+configFile_Path = str(Path(__file__).parent) + "\CDF_lib_path.txt" # location to directory that contains the dllcdf.dll needed for pycdf the library
+configfile = open(configFile_Path,'r')
+CDF_LIB =  configfile.readline()
